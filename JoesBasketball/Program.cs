8e4f09411a99fe98a52c38d2835679af844479d7
shivaamanaganti <shiva30jun@gmail.com>
2025-01-30 23:19:51 -0500

    Add project files.

---
+++ JoesBasketball/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+
+
+namespace JoesBasketball
+
+
+{
+    class Games
+
+
+    {
+        static void Main()
+        {
+
+            Console.WriteLine("Number of Purple tickets:");
+            int purpleTickets = int.Parse(Console.ReadLine());
+
+            if (purpleTickets <= 0) // The input of number purple tickets should be positive not negative
+            {
+                Console.WriteLine("Invalid input. Please must enter a positive number.");
+                return;
+            }
+
+
+
+            Console.WriteLine("Number of Green tickets:");
+            int greenTickets = int.Parse(Console.ReadLine());
+
+            if (greenTickets <= 0) // The input of number green tickets should be positive not negative
+            {
+                Console.WriteLine("Invalid input. Please must enter a positive number.");
+                return;
+            }
+
+
+            Console.WriteLine("Number of Blue tickets:");
+            int blueTickets = int.Parse(Console.ReadLine());
+
+            if (blueTickets <= 0) // The input of number blue tickets should be positive not negative
+            {
+                Console.WriteLine("Invalid input. Please must enter a positive number.");
+                return;
+            }
+
+
+            // Cost of each ticket according to each colour seat
+            int purpleticketPrice = 50;
+            int greenticketPrice = 80;
+            int blueticketPrice = 100;
+
+
+            int totalCost = (purpleTickets * purpleticketPrice) + (greenTickets * greenticketPrice) + (blueTickets * blueticketPrice);
+
+
+            int totalTickets = purpleTickets + greenTickets + blueTickets;
+            int averageCost = totalCost / totalTickets;
+
+
+            // Showing results
+            Console.WriteLine("Total money spent is: $" + totalCost);
+            Console.WriteLine("Average cost per each game is: $" + averageCost);
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ JoesBasketball/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("JoesBasketball")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP")]
+[assembly: AssemblyProduct("JoesBasketball")]
+[assembly: AssemblyCopyright("Copyright © HP 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c3feb099-6dec-479b-bc44-3707c1384e6f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
